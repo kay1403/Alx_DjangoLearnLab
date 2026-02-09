b2520559a6e724800dd2c13f22044b437a6fa855
Ange Koumba <angekoumba754@gmail.com>
2026-02-10 01:09:07 +0200

    security test2

---
+++ advanced_features_and_security/LibraryProject/bookshelf/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import book_list, create_book, edit_book, delete_book
+
+urlpatterns = [
+    path('books/', book_list, name='book_list'),
+    path('book/create/', create_book, name='create_book'),
+    path('book/edit/<int:pk>/', edit_book, name='edit_book'),
+    path('book/delete/<int:pk>/', delete_book, name='delete_book'),
+]
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,13 @@
 from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Book
+
+@permission_required('bookshelf.can_view', raise_exception=True)
+def book_list(request):
+    books = Book.objects.all()
+    return render(request, 'bookshelf/book_list.html', {'books': books})
+
 
 @permission_required('bookshelf.can_create', raise_exception=True)
 def create_book(request):
