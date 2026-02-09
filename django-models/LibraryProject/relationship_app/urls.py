a52e0059c1ccf1d2becf41e89b2b8b42f1f1ea77
Ange Koumba <angekoumba754@gmail.com>
2026-02-09 22:47:43 +0200

    urls fixing

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
+from .views import list_books
 from .views import (
-    list_books, LibraryDetailView,
+    LibraryDetailView,
     add_book, edit_book, delete_book,
     admin_view, librarian_view, member_view,
     register_view, login_view, logout_view
