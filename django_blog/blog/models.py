17e6be05c5b3ad7bd0b2c5e859823e17e8e5e70f
Ange Koumba <angekoumba754@gmail.com>
2026-02-19 18:24:28 +0200

    tag issues solving

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,13 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=50, unique=True)
-
-    def __str__(self):
-        return self.name
+from taggit.managers import TaggableManager
 
 
 class Post(models.Model):
@@ -15,7 +9,7 @@ class Post(models.Model):
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, blank=True)
+    tags = TaggableManager(blank=True)
 
     def __str__(self):
         return self.title
